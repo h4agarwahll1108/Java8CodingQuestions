d962de749891d6a4fb4fe8644a610367517007bb
DELL@Harshit <h4agarwahll@gmail.com>
2024-06-15 17:38:25 +0530

    Clean Code

---
+++ src/main/java/com/harshit1108/Concurrency/ProducerConsumer/ProducerConsumerExample.java
@@ -0,0 +1,38 @@
+package com.harshit1108.Concurrency.ProducerConsumer;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ProducerConsumerExample {
+    public static void main(String[] args) {
+        BlockingQueue<Integer> buffer = new ArrayBlockingQueue<>(5);
+
+        Thread producerThread = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    buffer.put(i); // This will block if the buffer is full.
+                    System.out.println("Produced " + i);
+                    Thread.sleep((long) (Math.random() * 5000));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, "Producer");
+
+        Thread consumerThread = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+
+                    int value = buffer.take(); // This will block if the buffer is empty.
+                    System.out.println("Consumed " + value);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, "Consumer");
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
